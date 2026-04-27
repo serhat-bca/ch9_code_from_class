24c6fbf78048175dc4427a9d84da59cfa4566948
serhat-bca <sersen@bergen.org>
2026-04-27 12:33:02 -0400

    useQuery added for tasks server state.

--- taskmanager-query/src/components/TaskList.jsx
+++ taskmanager-query/src/components/TaskList.jsx
@@ -1,6 +1,17 @@
 import Task from "./Task";
+import { useQuery } from "@tanstack/react-query";
+import { fetchTasks } from "../services/api";
 
-const TaskList = ({ tasks }) => {
+const TaskList = () => {
+  const {
+    data: tasks,
+    isLoading,
+    isError,
+  } = useQuery({ queryKey: ["tasks"], queryFn: fetchTasks });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error Loading data...</p>;
+
   return (
     <div>
       {tasks.map((task) => (
