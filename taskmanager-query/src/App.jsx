c6d3ad483c4ed2d00a4a968fcf015cb545c2fc0c
serhat-bca <sersen@bergen.org>
2026-04-27 12:16:56 -0400

    old style of fetching server state.

--- taskmanager-query/src/App.jsx
+++ taskmanager-query/src/App.jsx
@@ -1,13 +1,23 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import FilterTask from "./components/FilterTask";
+import { useState, useEffect } from "react";
+import { fetchTasks } from "./services/api";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks()
+      .then((data) => setTasks(data))
+      .catch();
+  }, []);
+
   return (
     <div>
       <h2>Tasks with Query and Context</h2>
       <FilterTask />
-      <TaskList />
+      <TaskList tasks={tasks} />
       <TaskForm />
     </div>
   );
--- taskmanager-query/src/components/TaskList.jsx
+++ taskmanager-query/src/components/TaskList.jsx
@@ -1,13 +1,6 @@
 import Task from "./Task";
 
-const TaskList = () => {
-  const tasks = [
-    { id: "1", description: "Finish the report", urgent: true },
-    { id: "2", description: "Buy Groceries", urgent: false },
-    { id: "3", description: "Call John", urgent: false },
-    { id: "4", description: "Pay Internet Bill", urgent: true },
-  ];
-
+const TaskList = ({ tasks }) => {
   return (
     <div>
       {tasks.map((task) => (
